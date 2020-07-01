58b99cf0333da4a2159bd14ea9d1371528308bc0
Jonas Scholz <jonas.scholz123@gmail.com>
2020-07-01 10:41:53 +0100

    imports training/test sets for agreement/disagreement detection

--- src/argue_importer.py
+++ src/argue_importer.py
@@ -1,10 +1,25 @@
-from iacorpus import load_dataset
+import pandas as pd
 
-dataset = load_dataset('createdebate')
-#print(dataset.dataset_metadata)
-for discussion in dataset:
-    print(discussion)
-    for post in discussion[0]:
-        print(post)
-        print(type(post))
-        exit()
+tasks1 = pd.read_csv("../data/argue_corpus/qr_worker_answers_task1.csv")
+tasks2 = pd.read_csv("../data/argue_corpus/qr_worker_answers_task2.csv")
+annotated = pd.read_csv("../data/argue_corpus/qr_meta.csv")
+
+posts = pd.read_csv("../data/argue_corpus/4forums.csv")
+
+print(max(posts["disc_id"]))
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+
+posts.head()
+
+
+tasks1.head()
+
+posts[posts["disc_id"] == 10].head(50)
+
+
+annotated = pd.merge(annotated, tasks1)
+annotated = pd.merge(annotated, tasks2)
+
+annotated.columns
+annotated[annotated["agreement"] == max(annotated["agreement"])]
