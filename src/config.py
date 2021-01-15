e35992f882775a5eb52ece29c0fe3a9277a417db
Jonas Scholz <jonas.scholz123@gmail.com>
2021-01-15 11:57:16 +0000

    add figure directory

--- src/config.py
+++ src/config.py
@@ -22,6 +22,7 @@ corpus = dict(
 paths = dict(
     transcripts = '../transcripts/',
     transcript_dfs = '../processed_transcripts/',
+    figures = '../figures',
 )
 
 # disable huge number of information prints by tensorflow
