14cc2e423d2e6ef6eedf27f412733158784c59b7
Jonas Scholz <jonas.scholz123@gmail.com>
2021-01-28 11:27:24 +0000

    fixed small visualisation bug by putting long topics at bottom

--- src/visualiser.py
+++ src/visualiser.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 # %matplotlib inline
 from matplotlib.patches import Circle, ConnectionPatch
+from utils import load_all_processed_transcripts
 import numpy as np
 import pandas as pd
 from topics import TopicExtractor
 import config
 from matplotlib import cm
 import matplotlib as mpl
+from tqdm import tqdm
 
 
 class Visualiser():
@@ -20,14 +22,12 @@ class Visualiser():
         self.nodes = []
 
         # position data
-        # self.y = 0
-        # self.delta_y = 1 #depth gained per timestep
         self.min_x = 0
         self.max_x = 0
         self.min_y = 0
         self.max_y = 0
 
-        self.delta_y = 2  # height difference between topics
+        self.delta_y = 3  # height difference between topics
 
         self.width = 10
 
@@ -53,7 +53,7 @@ class Visualiser():
         return node
 
     def add_section(self, x0, x1, s, color):
-        y = self.lowest_free_y[x0:x1].max() * self.delta_y
+        y = self.lowest_free_y[x0:x1].max()
 
         n0 = self.add_node(x0, y)
         n1 = self.add_node(x1, y)
@@ -83,7 +83,8 @@ class Visualiser():
         self.nr_objects[int(x0): int(xf)] += 1
 
         for i, lfy in enumerate(self.lowest_free_y[int(x0): int(xf)], int(x0)):
-            if lfy >= y0/self.delta_y:
+            # if lfy == y0/self.delta_y:
+            if lfy == y0:
                 self.lowest_free_y[i] += self.delta_y
 
         con = ConnectionPatch(n0.center, nf.center, "data", "data", zorder=0)
@@ -92,45 +93,59 @@ class Visualiser():
             con.set_edgecolor(color)
         self.ax.add_artist(con)
 
-# def invert_dict_of_lists(d):
-#     new_dict = defaultdict(list)
-#     for key, l in d.items():
-#         for listitem in l:
-#             new_dict[listitem].append(key)
-#
-#     return new_dict
 
+def get_topic_ranges(tdf):
+
+    trs = {}
+
+    for i, topics in tdf["topics"].iteritems():
+        for topic in topics:
+            existing_range = trs.get(frozenset(topic))
+            if (existing_range is not None and i >= existing_range[0]
+                    and i <= existing_range[1]):
+                continue
+            for j, next_topics in tdf.loc[i + 1:, "topics"].iteritems():
+                if topic in next_topics:
+                    continue
+                trs[frozenset(topic)] = (i, j - 1)
+                break
+    return trs
 
 def get_colormap(embeddings_1d):
     norm = mpl.colors.Normalize(embeddings_1d.min(), embeddings_1d.max())
     cmap = cm.tab20c
     return cm.ScalarMappable(norm=norm, cmap=cmap)
 
-
-te = TopicExtractor()
-# transcript_name = "joe_rogan_elon_musk"
-transcript_name = "sam_harris_nicholas_christakis"
-tdf = pd.read_pickle("../processed_transcripts/" + transcript_name + ".pkl")
-pd.options.display.max_rows = None
-embeds1d = np.array(te.fit_n_d_embeddings(tdf, 1))
-scalar_to_color = get_colormap(embeds1d)
 # %%
-tdf, topic_ranges = te.add_topics(tdf, return_topic_ranges=True)
-vis = Visualiser(len(tdf))
-min_topic_length = 5
+te = TopicExtractor()
+# %%
+transcript_name = "joe_rogan_elon_musk"
+# transcript_name = "sam_harris_nicholas_christakis"
+tdf = pd.read_pickle("../processed_transcripts/" + transcript_name + ".pkl")
 
-for tr, topics in topic_ranges.items():
-    if tr[1] - tr[0] < min_topic_length:
-        continue
+for tdf in tqdm(load_all_processed_transcripts()):
 
-    for topic in topics:
+    pd.options.display.max_rows = None
+    embeds1d = np.array(te.fit_n_d_embeddings(tdf, 1))
+    scalar_to_color = get_colormap(embeds1d)
+    #tdf, topic_ranges = te.add_topics(tdf, return_topic_ranges=True)
+    topic_ranges = get_topic_ranges(tdf)
+    vis = Visualiser(len(tdf))
+    min_topic_length = 1
+    # sort topics by tr, so that bigger ones placed at the bottom
+    tr_items = sorted(list(topic_ranges.items()),
+                      key=lambda x: x[1][1] - x[1][0],
+                      reverse=True)
+
+    for topic, tr in tr_items:
+        if tr[1] - tr[0] < min_topic_length:
+            continue
         joined = ", ".join(list(topic))
-
         embedding_1d = te.get_n_d_embedding(topic, 1)
         if embedding_1d is False:
             color = 'grey'
         color = scalar_to_color.to_rgba(embedding_1d)
         vis.add_section(tr[0], tr[1], joined, color)
 
-vis.save_fig(transcript_name)
-vis.show_fig()
+    vis.save_fig(transcript_name)
+    vis.show_fig()
