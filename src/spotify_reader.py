8186adb19deff575898ad72a9ea1652ee5db0d2c
Jonas Scholz <jonas.scholz123@gmail.com>
2021-02-01 11:25:52 +0000

    use metadata to make meaningful titles

--- src/spotify_reader.py
+++ src/spotify_reader.py
@@ -2,6 +2,10 @@ import json
 import os
 from datetime import timedelta
 import config
+import pandas as pd
+import re
+
+# TODO: use metadata to get real titles?
 
 def json_to_transcript(in_path, out_path):
     transcript_data = extract_json_transcript(in_path)
@@ -59,8 +63,21 @@ def write_transcript(path, utterances, speakers_and_times):
         f.writelines(lines)
     return
 
+def make_title(meta, hash):
+    entry = meta[meta['episode_filename_prefix'] == hash]
+    # limit title length
+    show_name = entry['show_name'].values[0].lower().replace(" ", "_")
+    # max 2 words, max 15 chars
+    show_name = "_".join(show_name.split("_")[:2])[:15]
+    ep_name = entry["episode_name"].values[0].lower().replace(" ", "_")
+    ep_name = "_".join(ep_name.split("_")[:2])[:15]
+    idx = entry.index[0]
+    title = "_".join(["spotify", show_name, ep_name, str(idx)])
+    return re.sub(r'\W+', '', title)    # filter non alphanumerical chars
 
 def extract_spotify(max_nr=None):
+
+    meta = pd.read_csv(config.paths["spotify_meta"], sep="\t")
 
     json_paths = []
 
@@ -78,7 +95,7 @@ def extract_spotify(max_nr=None):
     counter = 0
     for jp in json_paths:
         hash = jp.split("/")[-1].split(".")[0]
-        new_fn = "spotify_" + hash
+        new_fn = make_title(meta, hash)
         new_fp = config.paths["transcripts"] + new_fn + ".txt"
 
         if os.path.exists(new_fp):
@@ -87,7 +104,7 @@ def extract_spotify(max_nr=None):
         utterances, speakers_and_times = extract_json_transcript(jp)
 
         if len(utterances) <= config.data["max_nr_utterances"]:
-            #too short is bad
+            # shorter than one chunk is too short, filter
             continue
 
         write_transcript(new_fp, utterances, speakers_and_times)
@@ -105,3 +122,4 @@ def remove_all_spotify():
 
 
 extract_spotify(max_nr=50)
+#remove_all_spotify()
